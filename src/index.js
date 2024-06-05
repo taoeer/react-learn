9349aa7c7df80cb2a4d95bec6022c417a9b779b5
taoeer <719012229@qq.com>
2024-06-05 22:21:24 +0800

    init

---
+++ src/index.js
@@ -0,0 +1,11 @@
+import * as React from "react";
+import * as ReactDOM from "react-dom";
+import "./index.css";
+import App from "./App";
+
+ReactDOM.render(
+  <div className="App">
+  <p>hello world</p>
+</div>,
+  document.getElementById("root")
+);
